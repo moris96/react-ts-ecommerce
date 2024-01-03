f0844cca3e73a639e6db4a5edead513725b5f92d
moris96 <moriskhoudari@gmail.com>
2024-01-03 16:20:34 -0500

    added currency converter utility for items

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap"
+import { formatCurrency } from "../utilities/formatCurrency"
 
 type StoreItemProps = {
     id: number,
@@ -13,6 +14,12 @@ export function StoreItem({ id, name, price, imgURL }: StoreItemProps) {
         <>
         <Card>
             <Card.Img variant="top" src={imgURL} height="300px" style={{ objectFit: "cover" }} />
+            <Card.Body className="d-flex flex-column">
+                <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                    <span className="fs-2">{name}</span>
+                    <span className="ms-2 text-muted">{formatCurrency(price)}</span>
+                </Card.Title>
+            </Card.Body>
         </Card>
         </>
     )
