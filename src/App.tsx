5fa9d326018d4357c6520b9680ba1cf66d40973e
moris96 <moriskhoudari@gmail.com>
2024-01-04 15:33:33 -0500

    store is homepage for now until the other pages are built

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,20 @@ import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
 import { About } from "./pages/About";
 
-import { NavBar } from "./components/Navbar";
+// import { NavBar } from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
+import { NavBar } from "./components/NavBar";
 
 
 function App() {
   return (
     <ShoppingCartProvider>
-      <NavBar/>
+      {/* <NavBar/> */}
+      <NavBar />
       <Container className="mb-4">
         <Routes>
-          <Route path="/" element={ <Home/> } />
-          <Route path="/store" element={ <Store/> } />
+          <Route path="/home" element={ <Home/> } />
+          <Route path="/" element={ <Store/> } />
           <Route path="/about" element={ <About/> } />
         </Routes>
       </Container>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,8 +11,10 @@ export function NavBar() {
         <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
             <Container>
                 <Nav className="me-auto">
-                    <Nav.Link to={"/"} as={NavLink}>Home</Nav.Link>
-                    <Nav.Link to={"/store"} as={NavLink}>Store</Nav.Link>
+                    {/* <Nav.Link to={"/"} as={NavLink}>Home</Nav.Link>
+                    <Nav.Link to={"/store"} as={NavLink}>Store</Nav.Link> */}
+                    <Nav.Link to={"/"} as={NavLink}>Store</Nav.Link>
+                    <Nav.Link to={"/home"} as={NavLink}>Home</Nav.Link>
                     <Nav.Link to={"/about"} as={NavLink}>About</Nav.Link>
                 </Nav>
                 {cartQuantity > 0 && (
