0be5386df0d5332428d508778c3360cc9ca3a1f5
moris96 <moriskhoudari@gmail.com>
2024-01-04 15:17:39 -0500

    shopping cart component updated so that it shows item quantity, price, total price

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, createContext, useContext, useState } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
 
 type ShoppingCartProviderProps = {
     children: ReactNode
@@ -83,6 +84,7 @@ export function ShoppingCartProvider( { children }: ShoppingCartProviderProps ) 
     return (
         <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart, openCart, closeCart, cartItems, cartQuantity, }}>
             {children}
+            <ShoppingCart isOpen={isOpen}/>
         </ShoppingCartContext.Provider>
     );
 };
